304ebd679bc0561c44af88ab13f200f3fcd0ae41
kielkow <matheuskiel@fiorifer.com.br>
2022-03-28 11:43:36 -0300

    test: should be able to get a statement

---
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -0,0 +1,76 @@
+import { InMemoryStatementsRepository } from "@modules/statements/repositories/in-memory/InMemoryStatementsRepository";
+import { InMemoryUsersRepository } from "@modules/users/repositories/in-memory/InMemoryUsersRepository";
+
+import { AuthenticateUserUseCase } from "@modules/users/useCases/authenticateUser/AuthenticateUserUseCase";
+import { CreateUserUseCase } from "@modules/users/useCases/createUser/CreateUserUseCase";
+
+import { ICreateUserDTO } from "@modules/users/useCases/createUser/ICreateUserDTO";
+import { AppError } from "@shared/errors/AppError";
+import { CreateStatementUseCase } from "../createStatement/CreateStatementUseCase";
+import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createStatementUseCase: CreateStatementUseCase;
+let getStatementOperationUseCase: GetStatementOperationUseCase
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let statementsRepositoryInMemory: InMemoryStatementsRepository;
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+describe('Get an Statement', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    statementsRepositoryInMemory = new InMemoryStatementsRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory,
+    );
+
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    createStatementUseCase = new CreateStatementUseCase(
+      usersRepositoryInMemory,
+      statementsRepositoryInMemory
+    );
+    getStatementOperationUseCase = new GetStatementOperationUseCase(
+      usersRepositoryInMemory,
+      statementsRepositoryInMemory
+    );
+  });
+
+  it('should be able to get a statement', async () => {
+    const user: ICreateUserDTO = {
+      name: 'jonh doe',
+      email: 'jonhdoe@email.com',
+      password: '123456',
+    };
+
+    await createUserUseCase.execute(user);
+
+    const authenticationInfo = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    const statementOfDeposit = await createStatementUseCase.execute({
+      user_id: authenticationInfo.user.id,
+      amount: 100,
+      description: 'test of deposit',
+      type: OperationType.DEPOSIT
+    });
+
+    const statement = await getStatementOperationUseCase.execute({
+      user_id: authenticationInfo.user.id,
+      statement_id: statementOfDeposit.id
+    });
+
+    expect(statement).toHaveProperty('id');
+    expect(statement.user_id).toEqual(authenticationInfo.user.id);
+    expect(statement.description).toEqual('test of deposit');
+    expect(statement.type).toEqual('deposit');
+    expect(statement.amount).toEqual(100);
+  });
+});
