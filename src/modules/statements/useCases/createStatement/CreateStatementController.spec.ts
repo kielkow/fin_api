944aaaadbafa627348c5957b5f8c300d694b4382
kielkow <matheuskiel@fiorifer.com.br>
2022-04-04 10:57:09 -0300

    test: should be able to create an statement of withdraw

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -49,4 +49,36 @@ describe('Create Statement Controller', () => {
         expect(response.body.type).toEqual('deposit');
         expect(response.body.amount).toEqual(100);
     });
+
+    it('should be able to create an statement of withdraw from an user', async () => {
+        const { body: authInfo } = await request(app).post('/api/v1/sessions').send({
+            email: 'admin@finapi.com.br',
+            password: 'admin',
+        });
+
+        await request(app).post('/api/v1/statements/deposit')
+            .set({
+                Authorization: `Bearer ${authInfo.token}`,
+            })
+            .send({
+                amount: 100,
+                description: 'test of deposit',
+            });
+
+        const response = await request(app).post('/api/v1/statements/withdraw')
+            .set({
+                Authorization: `Bearer ${authInfo.token}`,
+            })
+            .send({
+                amount: 50,
+                description: 'test of withdraw',
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.user_id).toEqual(authInfo.user.id);
+        expect(response.body.description).toEqual('test of withdraw');
+        expect(response.body.type).toEqual('withdraw');
+        expect(response.body.amount).toEqual(50);
+    });
 });
