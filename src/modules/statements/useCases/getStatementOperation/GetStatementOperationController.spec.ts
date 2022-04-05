ae2f9ec3fe51fe9c6dcb0524cdbac0f1910a635c
kielkow <matheuskiel@fiorifer.com.br>
2022-04-05 10:42:44 -0300

    test: should be able to get info about statement operation by the controller

---
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import { hash } from "bcryptjs";
+import { v4 as uuid } from 'uuid';
+import { Connection } from "typeorm";
+
+import { app } from '../../../../app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+describe('Get Statement Operation Controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuid();
+        const password = await hash('admin', 8);
+
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, created_at ) 
+        values('${id}', 'admin', 'admin@finapi.com.br', '${password}', 'now()')
+      `,
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it('should be able to get info about an statement operation', async () => {
+        const { body: authInfo } = await request(app).post('/api/v1/sessions').send({
+            email: 'admin@finapi.com.br',
+            password: 'admin',
+        });
+
+        const { body: statement } = await request(app).post('/api/v1/statements/deposit')
+            .set({
+                Authorization: `Bearer ${authInfo.token}`,
+            })
+            .send({
+                amount: 100,
+                description: 'test of deposit',
+            });
+
+        const response = await request(app).get(`/api/v1/statements/${statement.id}`).set({
+            Authorization: `Bearer ${authInfo.token}`,
+        });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.user_id).toEqual(authInfo.user.id);
+        expect(response.body.description).toEqual('test of deposit');
+        expect(response.body.type).toEqual('deposit');
+        expect(Number(response.body.amount)).toEqual(100);
+    });
+});
